c0e8cb8fae28318b70dfb442243ccc599acdcbae
Travis Tai <ttai2@alumni.uwo.ca>
2026-02-24 12:22:39 -0800

    Test cpue function

---
+++ tests/testthat/test-cpue.R
@@ -0,0 +1,26 @@
+test_that("cpue calculates simple ratio correctly", {
+  expect_equal(cpue(catch = 100, effort = 10), 10)
+  expect_equal(cpue(catch = 50, effort = 2), 25)
+})
+
+test_that("cpue works with vectors of data", {
+  catches <- c(100, 200, 300)
+  efforts <- c(10, 10, 10)
+
+  expected_results <- c(10, 20, 30)
+  expect_equal(cpue(catches, efforts), expected_results)
+})
+
+test_that("cpue returns numeric values", {
+  expect_type(cpue(100,10), "double")
+})
+
+test_that("gear_factor standardization scales correctly", {
+  expect_equal(cpue(catch = 100, effort = 10, gear_factor = 0.5), 5)
+
+  # Default gear_factor = 1 should have no effect
+  expect_equal(
+    cpue(catch = 100, effort = 10),
+    cpue(catch = 100, effort = 10, gear_factor = 1)
+  )
+})
