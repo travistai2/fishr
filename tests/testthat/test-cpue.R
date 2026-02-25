0decc5699f1b596d1a8fbca753099430d545f810
Travis Tai <ttai2@alumni.uwo.ca>
2026-02-25 08:16:15 -0800

    Test for NA values in CPUE

--- tests/testthat/test-cpue.R
+++ tests/testthat/test-cpue.R
@@ -24,3 +24,8 @@ test_that("gear_factor standardization scales correctly", {
     cpue(catch = 100, effort = 10, gear_factor = 1)
   )
 })
+
+test_that("cpue handles missing data", {
+  expect_true(is.na(cpue(NA_real_, 10)))
+  expect_true(is.na(cpue(100, NA_real_)))
+})
